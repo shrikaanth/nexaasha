11b7faf87d7573dfaf0984862a2680a6d08e5264
Shrikaanth Shyamsunder <s.shrikaanth@gmail.com>
2025-07-08 00:38:48 +0530

    added files

--- index.php
+++ index.php
@@ -20,9 +20,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     
     <!-- Custom CSS -->
-     <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon_io/apple-touch-icon.png">
+ <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon_io/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon_io/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon_io/favicon-16x16.png">
+<link rel="manifest" href="assets/img/favicon_io/site.webmanifest">
+
 <link rel="manifest" href="assets/img/favicon_io/site.webmanifest">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
